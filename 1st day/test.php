4a3f18f1e04b6471c9ebdd161dcafa31af9cfe79
Yerkebulan <erke.zhumagali@gmail.com>
2018-06-13 12:38:39 +0600

    first commit

---
+++ 1st day/test.php
@@ -0,0 +1,52 @@
+<?php
+
+
+$array = [
+	0 => '1';
+	1 => '2';
+	2 => '3';
+	3 => '4';
+];
+
+foreach($array as $key => value) {
+	if($ key == 2) {
+		var_dump($value);
+	}
+}
+
+/*$var = 'Hello world';
+$var = 25;
+$var = ['1', '2', '3'];
+
+define('TESTVAR', 123);
+
+//echo $var
+
+//var_dump($var);
+
+echo(TESTVAR);
+
+$color = 'red'; */
+/*if($color == 'green') {
+	echo 'Green color';
+}	else {
+	echo 'Not green color';
+} */
+
+/* switch () {
+	case 'value':
+		# code...
+		break;
+	
+	default:
+		# code...
+		break;
+} */
+
+/*$var = 1;
+while ($i <= 10){
+	echo $i;
+	$var++;
+} */
+
+?>
